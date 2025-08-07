05979fc34db19198f602aaff2f0b0461ef86752a
lianthompson <lianythompson@gmail.com>
2025-08-06 22:19:12 -0700

    input logic

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,43 @@
+'use client';
+import React, { useState } from 'react';
 import Input from "../input";
 
+interface ContactFormData {
+    name: string,
+    email: string,
+    subject: string,
+    message: string
+}
+
 export default function Contact() {
+
+    const [data, setFormData] = useState<ContactFormData>({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+
+    const handleInputChange = (type, e) => {
+        setFormData(prev => ({ ...prev, [type]: e}));
+    }
+
+    const handleSubmit = () => {
+        setFormData({
+            'name': '',
+            'email': '',
+            'subject': '',
+            'message': ''
+        });
+    }
+
     return (
         <div className="flex flex-col justify-end justify-center m-auto font-[family-name:var(--font-molengo)] text-[#3B3B93] gap-[20px]">
-            <Input title={"Name"} type={"text"}></Input>
-            <Input title={"Email"} type={"email"}></Input>
-            <Input title={"Subject"} type={"text"}></Input>
-            <Input title={"Message"} type={"textarea"}></Input>
+            <Input title={"Name"} type={"text"} onChange={(e) => handleInputChange('name', e)} value={data.name}></Input>
+            <Input title={"Email"} type={"email"} onChange={(e) => handleInputChange('email', e)} value={data.email}></Input>
+            <Input title={"Subject"} type={"text"} onChange={(e) => handleInputChange('subject', e)} value={data.subject}></Input>
+            <Input title={"Message"} type={"textarea"} onChange={(e) => handleInputChange('message', e)} value={data.message}></Input>
+            <button onClick={handleSubmit}>Submit</button>
             <div className="text-3xl">+1 415 342 2582</div>
         </div>
     );
--- app/input.tsx
+++ app/input.tsx
@@ -1,20 +1,31 @@
+'use client';
+import React, { useState } from 'react';
 import cn from 'classnames';
 
 interface InputProps {
     title: string,
-    type: string
+    type: string,
+    value: string,
+    onChange: () => void
 }
 
-export default function Input({ title, type }: InputProps) {
-    const inputClasses = cn("border-2 border-indigo-100/75 rounded-[8px] !focus:border-pink-600 h-[48px] w-[400px]");
-    const titleClass = "text-3xl mb-[8px]";
+export default function Input({ title, type, value, onChange }: InputProps) {
+
+    const handleInputChange = (e) => {
+        onChange(e.target.value)
+    }
+
+    const inputClasses = cn("border-2 border-indigo-100/75 rounded-[8px] focus:outline-hidden focus-visible:border-[#3B3B93] h-[48px] w-[400px] pl-[20px] pr-[20px]");
+    const titleClass = "text-2xl mb-[8px]";
     return <div className="font-[family-name:var(--font-molengo)] text-[#3B3B93]">
         {type !== "textarea" && <div>
-            <div className={titleClass}>{title}</div><input type={type} className={inputClasses}></input></div>
+            <div className={titleClass}>{title}</div>
+            <input type={type} className={inputClasses} onChange={handleInputChange} value={value}></input>
+        </div>
         }
         {type === "textarea" && <div>
             <div className={titleClass}>{title}</div>
-            <textarea className={cn(inputClasses, "h-[200px]")}></textarea>
+            <textarea className={cn(inputClasses, "h-[200px] pt-[12px] pb-[12px]")} onChange={handleInputChange} value={value}></textarea>
         </div>
         }
     </div>
